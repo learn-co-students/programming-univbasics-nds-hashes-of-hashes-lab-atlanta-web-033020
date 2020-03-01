1a4ebaaec64fec2cb20326f23b11c9c1a740f5f7
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-01 16:42:21 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,9 +48,19 @@ def naming_system
         sub_category: {
              label: "Phylum",
              sub_category: {
-               label: "Class"
+               label: "Class",
                sub_category: {
-                 label: "Order"
+                 label: "Order",
+                 sub_category: {
+                   label: "Family",
+                   sub_category: {
+                     label: "Genus",
+                     sub_category: {
+                       label: "Species"
+                    #  sub_category: {}
+                     }
+                   }
+                 }
                }
              }
         }
